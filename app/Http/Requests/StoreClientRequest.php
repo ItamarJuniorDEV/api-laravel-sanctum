a7b1c1a1c62a1405842bd1777463ac9972c3038e
Itamar Junior <itamar.junior@irriga.net>
2026-03-17 18:16:30 -0300

    feat: Criei os Form Requests de login e de clientes.

---
+++ app/Http/Requests/StoreClientRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreClientRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:clients,email',
+            'phone' => 'required|string|max:20'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome é obrigatório',
+            'email.required' => 'O e-mail é obrigatório',
+            'email.email' => 'Digite um e-mail válido',
+            'email.unique' => 'Este e-mail já está cadastrado',
+            'phone.required' => 'O telefone é obrigatório'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status_code' => 422,
+            'message' => 'Dados inválidos',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
