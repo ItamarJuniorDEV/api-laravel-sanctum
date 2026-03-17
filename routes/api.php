609719d1a7d1d1f442e983b95fefed850981e9b0
Itamar Junior <itamar.junior@irriga.net>
2026-03-17 18:12:35 -0300

    fix: Removi rotas comentadas e o import do Request que não estava sendo usado.

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use App\Services\ApiResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/status', function() {
@@ -11,11 +10,6 @@ Route::get('/status', function() {
 })->middleware('auth:sanctum');
 
 Route::apiResource('clients', ClientController::class)->middleware('auth:sanctum');
-// Route::get('clients', [ClientController::class, 'index'])->middleware(['auth:sanctum', 'abilities:clients:list']);
-// Route::post('clients', [ClientController::class, 'store'])->middleware(['auth:sanctum', 'abilities:clients:detail']);
-// Route::get('clients/{id}', [ClientController::class, 'show']);
-// Route::put('clients/{id}', [ClientController::class, 'update']);
-// Route::delete('clients/{id}', [ClientController::class, 'destroy']);
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');;
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
