478c8f753da40c59003bebb5537a57f5ff3b6e64
Itamar Junior <itamar.junior@irriga.net>
2026-03-17 18:12:32 -0300

    fix: Padronizei as verificações de ability em todos os métodos do ClientController.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,86 +8,84 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        // if(!auth()->user()->tokenCan('clients:list')) {
-        //    return ApiResponse::error('Acesso negado', 401);
-        // }
-        //
+        if (!auth()->user()->tokenCan('clients:list')) {
+            return ApiResponse::error('Acesso negado', 401);
+        }
+
         return ApiResponse::success(Client::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
+        if (!auth()->user()->tokenCan('clients:detail')) {
+            return ApiResponse::error('Acesso negado', 401);
+        }
+
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:clients',
             'phone' => 'required'
-        ]); 
+        ]);
 
         $client = Client::create($request->all());
 
         return ApiResponse::success($client);
-        
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
-        if(!auth()->user()->tokenCan('clients:detail')) {
-            return ApiResponse::error('Acesso negado', 401);    
+        if (!auth()->user()->tokenCan('clients:detail')) {
+            return ApiResponse::error('Acesso negado', 401);
         }
-        
+
         $client = Client::find($id);
 
-        if ($client) {
-            return ApiResponse::success($client);
-        } else {
+        if (!$client) {
             return ApiResponse::error('Cliente não encontrado', 404);
         }
+
+        return ApiResponse::success($client);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
+        if (!auth()->user()->tokenCan('clients:detail')) {
+            return ApiResponse::error('Acesso negado', 401);
+        }
+
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:clients,email,'.$id,
             'phone' => 'required'
-        ]); 
+        ]);
 
         $client = Client::find($id);
 
-        if ($client) {
-            $client->update($request->all());
-            return ApiResponse::success($client);
-        } else {
-            return ApiResponse::error('Cliente não encontrado');
+        if (!$client) {
+            return ApiResponse::error('Cliente não encontrado', 404);
         }
+
+        $client->update($request->all());
+
+        return ApiResponse::success($client);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
+        if (!auth()->user()->tokenCan('clients:detail')) {
+            return ApiResponse::error('Acesso negado', 401);
+        }
+
         $client = Client::find($id);
 
-        if ($client) {
-            $client->delete();
-            return ApiResponse::success('Cliente deletado com sucesso');
-        } else {
-            return ApiResponse::error('Cliente não encontrado');
+        if (!$client) {
+            return ApiResponse::error('Cliente não encontrado', 404);
         }
+
+        $client->delete();
+
+        return ApiResponse::success('Cliente deletado com sucesso');
     }
 }
